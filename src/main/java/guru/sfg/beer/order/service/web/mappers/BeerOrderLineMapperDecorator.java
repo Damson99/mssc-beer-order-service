6d3e20718d35055d0adc47b0dafa3abe5cf8d073
DaMSoN <damian.wieczorek99@gmail.com>
2020-08-22 21:00:59 +0200

    fixing error. Closes #6

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperDecorator.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperDecorator.java
@@ -4,28 +4,18 @@ import guru.sfg.beer.order.service.domain.BeerOrderLine;
 import guru.sfg.beer.order.service.services.beer.BeerService;
 import guru.sfg.brewery.model.BeerDto;
 import guru.sfg.brewery.model.BeerOrderLineDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public abstract class BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
 
-    private BeerService beerService;
-    private BeerOrderLineMapper beerOrderLineMapper;
+    private final BeerService beerService;
+    private final BeerOrderLineMapper beerOrderLineMapper;
 
-    @Autowired
-    public void setBeerService(BeerService beerService)
-    {
-        this.beerService = beerService;
-    }
-
-    @Autowired
-    @Qualifier("delegate")
-    public void setBeerOrderLineMapper(BeerOrderLineMapper beerOrderLineMapper)
-    {
-        this.beerOrderLineMapper = beerOrderLineMapper;
-    }
 
     @Override
     public BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line)
@@ -33,7 +23,8 @@ public abstract class BeerOrderLineMapperDecorator implements BeerOrderLineMappe
         BeerOrderLineDto orderLineDto = beerOrderLineMapper.beerOrderLineToDto(line);
         Optional<BeerDto> beerDtoOptional = beerService.getBeerByUpc(line.getUpc());
 
-        beerDtoOptional.ifPresent(beerDto -> {
+        beerDtoOptional.ifPresent(beerDto ->
+        {
             orderLineDto.setBeerName(beerDto.getBeerName());
             orderLineDto.setBeerStyle(beerDto.getBeerStyle());
             orderLineDto.setPrice(beerDto.getPrice());
