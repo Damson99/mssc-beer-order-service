e67512906b45400e5dde2998a2f0509d227b669c
DaMSoN <damian.wieczorek99@gmail.com>
2020-08-30 01:47:52 +0200

    adding enum for Spring State Machine. Closes #21

--- src/main/java/guru/sfg/brewery/model/BeerPagedList.java
+++ src/main/java/guru/sfg/brewery/model/BeerPagedList.java
@@ -1,6 +1,5 @@
 package guru.sfg.brewery.model;
 
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
