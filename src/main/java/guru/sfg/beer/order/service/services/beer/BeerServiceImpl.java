4185b76a182bad3f01550c6e6e4dc3601bd50bd3
DaMSoN <damian.wieczorek99@gmail.com>
2020-08-30 20:33:14 +0200

    fixing path prefix for configuration props. Closes #22

--- src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Optional;
 import java.util.UUID;
 
-@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "sfg.brewery.beer-service-host", ignoreUnknownFields = false)
 @Service
 public class BeerServiceImpl implements BeerService
 {
