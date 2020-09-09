312dd8bd2cddcb24d677386d27c01cf5464166ce
DaMSoN <damian.wieczorek99@gmail.com>
2020-09-09 15:19:19 +0200

    adding deallocate order action. Closes #38

--- src/main/java/mssc/beer/sm/action/DeallocateOrderAction.java
+++ src/main/java/mssc/beer/sm/action/DeallocateOrderAction.java
@@ -2,12 +2,21 @@ package mssc.beer.sm.action;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mssc.beer.config.JmsConfig;
+import mssc.beer.domain.BeerOrder;
 import mssc.beer.domain.BeerOrderEventEnum;
 import mssc.beer.domain.BeerOrderStatusEnum;
+import mssc.beer.repositories.BeerOrderRepository;
+import mssc.beer.services.BeerOrderManagerImpl;
+import mssc.beer.web.mappers.BeerOrderMapper;
+import mssc.model.event.DeallocateOrderRequest;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -15,11 +24,23 @@ import org.springframework.stereotype.Component;
 public class DeallocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum>
 {
     private final JmsTemplate jmsTemplate;
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
 
     @Override
     public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext)
     {
+        String orderId = (String) stateContext.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(UUID.fromString(orderId));
 
+        beerOrderOptional.ifPresentOrElse(beerOrder ->
+        {
+            jmsTemplate.convertAndSend(JmsConfig.DEALLOCATE_ORDER_QUEUE,
+                    DeallocateOrderRequest.builder()
+                            .beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder)).build());
+
+            log.debug("Sent deallocation request for orderId: " + orderId);
+        }, () -> log.debug("Order not found. Id: " + orderId));
     }
 }
 
